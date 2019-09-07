05fac8186b1ac129f6f0ddc22e8aafa4c9d08ef4
hacknasa <giorgi.khatchapuridze.1@btu.edu.ge>
2019-09-08 00:33:55 +0400

    init commit :alien:

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/footer.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  box-sizing: border-box;
+
+
+  ul {
+    padding: 0;
+  }
+}
+</style>
---
+++ src/components/footer.vue
@@ -0,0 +1,96 @@
+<template>
+<footer>
+  <div class="footer-section">
+    <div class="footer-section__top">
+          <div class="footer-section__column">
+            <h6 class="footer-section__title">ჩემი ანგარიში</h6>
+                <ul>
+                  <li class="footer-section__list-item"><a href="">კარიერა</a></li>
+                  <li class="footer-section__list-item"><a href="">გუნდი</a></li>
+                  
+                </ul>
+          </div>
+          <div class="footer-section__column">
+            <h6 class="footer-section__title">დაიწყე გაყიდვა ჩვენთან</h6>
+                <ul>
+                  <li class="footer-section__list-item"><a href="">გადახდის საშუალებები</a></li>
+                  <li class="footer-section__list-item"><a href="">გადახდის საშუალებები</a></li>
+                  <li class="footer-section__list-item">\<a href="">გადახდის საშუალებები</a></li>
+                </ul>
+                
+
+          </div>
+          <div class="footer-section__column">
+            <h6 class="footer-section__title">პირობები</h6>
+                <ul>
+                  <li class="footer-section__list-item"><a href="">მონაცემების უსაფრთხოება</a></li>
+                  <li class="footer-section__list-item"><a href="">გადახდის საშუალებები</a></li>
+                  <li class="footer-section__list-item"><a href="">გადახდის საშუალებები</a></li>
+                </ul>
+
+          </div>
+          <div class="footer-section__column">
+            <h6 class="footer-section__title">ბლოგი</h6>
+                <ul>
+                  <li class="footer-section__list-item"><a href="">დაბრუნების პოლიტიკა</a></li>
+                  <li class="footer-section__list-item"><a href="">მიტანის სერვისი</a></li>
+                  <li class="footer-section__list-item"><a href="">გადახდის საშუალებები</a> </li>
+                </ul>
+
+          </div>
+
+               <div class="footer-section__blog">
+                   <h6 class="footer-section__title">ბლოგი</h6>
+                  <div class="footer-section__blog__blog-article">
+                      <img  class="footer-section__blog__blog-article__blogimage" src="../assets/Image_86.png" alt="sda"><a  class="blog-link" href="">როგორ შევიძინოთ ტექნიკა ვენდუს საიტზე</a>
+                  </div>
+
+                  <div class="footer-section__blog__blog-article">
+                      <img class="footer-section__blog__blog-article__blogimage" src="../assets/Image_87.png" alt="sda"><a class="blog-link" href="">როგორ შევიძინოთ ტექნიკა ვენდუს საიტზე</a>
+                  </div>
+               </div>
+          </div>
+
+    <div class="footer-section__bottom">
+      <div class="footer-section__row">
+          <p class="footer-section__copyright">©2019, ყველა უფლება დაცულია </p>
+        <div class="footer-section__bottom__icons">
+
+          <a href="https://www.youtube.com/channel/UC2bspBeXgd0VAI93srGSWtg" target="_blank">
+            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+          </a>
+          <a href="https://www.facebook.com/Vendoo.ge/" target="_blank">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+
+          <a href="https://www.facebook.com/Vendoo.ge/" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+          
+        </div>
+      <div class="footer-section__bottom__tbc">
+        <span>Powerd by:</span>
+        <a href="https://tbcbank.ge" target="_blank">
+        <img src="../assets/Image_61.png" alt="tbc bitch">
+        </a>
+      </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
